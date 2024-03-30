9b9ab0885774881b78a28e0c2b6df1bb1c850503
abi-hong <flffjakfwm20@gmail.com>
2024-03-31 00:46:53 +0900

    feat(service): Date 서비스 생성

--- shared/services/global.service.ts
+++ shared/services/global.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from "@angular/core";
 import { LayoutService } from "./layout.service";
 import { HttpService } from "./http.service";
+import { DateService } from "./date.service";
 
 @Injectable({ providedIn: 'root' })
 export class GlobalService {
     constructor(
+        public dateService: DateService,
         public httpService: HttpService,
         public layoutService: LayoutService,
-    ) {}
-    
+    ) { }
+
 }

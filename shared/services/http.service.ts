9ad1be637c433200a0e04e01e5339fc0764ca921
abi-hong <flffjakfwm20@gmail.com>
2024-04-01 22:47:36 +0900

    feat(http): snack-bar 생성

--- shared/services/http.service.ts
+++ shared/services/http.service.ts
@@ -1,6 +1,19 @@
 import { Injectable } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Injectable({ providedIn: 'root' })
 export class HttpService {
-    
+
+  constructor(private _snackBar: MatSnackBar) {
+
+  }
+
+  public snackBar(text: string, undo: string = "", duration: number = 2000) {
+    this._snackBar.open(text, undo, {
+      duration: duration,
+      horizontalPosition: "right",
+      verticalPosition: "top",
+    });
+  }
+
 }

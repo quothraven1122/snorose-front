b7855d25b625fe2fa0cda3ebbfec7fc7ace6da4a
abi-hong <flffjakfwm20@gmail.com>
2024-04-20 15:27:50 +0900

    feat(review): 리뷰 관련 http api 정리

---
+++ shared/http/review.http.ts
@@ -0,0 +1,131 @@
+import { Observable } from "rxjs";
+import { IBaseResponse } from "./baseResponse.dto";
+import { HttpService } from "../services/http.service";
+
+export enum Semester {
+  First = 'FIRST', // 1학기 
+  Second = 'SECOND', // 2학기
+  Summer = 'SUMMER',	// 여름학기
+  Winter = 'WINTER', // 겨울학기
+  Other = 'OTHER', 	// 기타
+}
+
+export enum LectureType {
+  MajorRequired = 'MAJOR_REQUIRED',	// 전공필수
+  MajorElective = 'MAJOR_ELECTIVE',	// 전공선택
+  GeneralRequired = 'GENERAL_REQUIRED',	// 교양 필수
+  GeneralElective = 'GENERAL_ELECTIVE',	// 교양 선택
+  Other = 'OTHER',	// 기타
+}
+
+export interface IReviewCreateRequest {
+  // 게시글 정보
+  boardId: number;
+  userDisplay: string;
+  category: string;
+  title: string;
+  content: string;
+
+  // 강의 정보
+  lectureName: string;
+  professor: string;
+  classNumber: number;
+  lectureYear: number;
+  semester: Semester;
+  hasMidterm: boolean;
+  hasFinalterm: boolean;
+  lectureType: LectureType;
+  isPF: boolean;
+
+  // 파일 정보
+  filePath: string;
+  fileName: string;
+}
+
+export interface IReviewUpdateRequest {
+  // 게시글 정보
+  category: string;
+  title: string;
+  content: string;
+
+  // 강의 정보
+  lectureName: string;
+  professor: string;
+  classNumber: number;
+  lectureYear: number;
+  semester: Semester;
+  hasMidterm: boolean;
+  hasFinalterm: boolean;
+  lectureType: LectureType;
+  isPF: boolean;
+
+  // 파일 정보
+  filePath: string;
+  fileName: string;
+}
+
+export interface IReviewListResponse extends IBaseResponse<Array<{
+  postId: number,
+  title: string;
+  likeCount: number;
+  viewCount: number;
+  createdAt: string;
+}>> { }
+
+export interface IReviewGetData {
+  userDisplay: string;
+  title: string;
+  content: string;
+
+  // 게시물 정보
+  viewCount: number;
+  scrapCount: number;
+  likeCount: number;
+  createdAt: string;
+
+  // 강의 정보
+  lectureName: string;
+  professor: string;
+  classNumber: number;
+  lectureYear: number;
+  semester: Semester;
+  lectureType: LectureType;
+  isPF: boolean;
+
+  // 파일 정보
+  filePath: string;
+  fileName: string;
+}
+
+export interface IReviewDetailResponse extends IBaseResponse<IReviewGetData> { }
+export interface IReviewCreateResponse extends IBaseResponse<{ postId: number; }> { }
+export interface IReviewUpdateResponse extends IBaseResponse<{ postId: number; }> { }
+
+export interface IReviewHttp {
+  getList(page: number): Observable<IReviewListResponse>;
+  getDetail(boardId: number, postId: number): Observable<IReviewDetailResponse>;
+  create(request: IReviewCreateRequest): Observable<IReviewCreateResponse>;
+  update(postId: number, request: IReviewUpdateRequest): Observable<IReviewUpdateResponse>;
+}
+
+export class ReviewHttp implements IReviewHttp {
+
+  constructor(private readonly httpService: HttpService) { }
+
+  public getList(page: number): Observable<IReviewListResponse> {
+    return this.httpService.Get(`/api/v1/reviews/list/${page}`);
+  }
+
+  public getDetail(boardId: number, postId: number): Observable<IReviewDetailResponse> {
+    return this.httpService.Get(`/api/v1/reviews/${boardId}/review/${postId}`);
+  }
+
+  public create(request: IReviewCreateRequest): Observable<IReviewCreateResponse> {
+    return this.httpService.Post(`/api/v1/reviews/review`, request);
+  }
+
+  public update(postId: number, request: IReviewUpdateRequest): Observable<IReviewUpdateResponse> {
+    return this.httpService.Patch(`/api/v1/reviews/${postId}`, request);
+  }
+
+}

5a5e507f6a71cda3cbdc97bda2103c07296fcf3b
abi-hong <flffjakfwm20@gmail.com>
2024-04-20 14:05:19 +0900

    feat(global): array protptype 선언

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,28 @@
 import { Component } from '@angular/core';
 
+declare global {
+
+  interface Array<T> {
+    last(): any;
+    first(): any;
+    isEmpty(): boolean;
+  }
+
+}
+
+Array.prototype.last = function () {
+  return this[this.length - 1];
+}
+
+Array.prototype.first = function () {
+  return this[0];
+}
+
+Array.prototype.isEmpty = function () {
+  if (this.length === 0) return true;
+  else return false;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',

c38b815b019c39301d8c1930f76365019505f3ee
abi-hong <flffjakfwm20@gmail.com>
2024-04-20 21:33:45 +0900

    feat(membership): user 데이터 함수 생성

--- shared/services/membership.service.ts
+++ shared/services/membership.service.ts
@@ -1,10 +1,43 @@
 import { Injectable } from "@angular/core";
+import { ISingInData } from "../http/membership.http";
 
 @Injectable({ providedIn: 'root' })
 export class MembershipService {
 
-  constructor() {
+  public initial: ISingInData = {
+    grantType: '',
+    accessToken: '',
+    refreshToken: '',
+  }
 
+  public data: ISingInData = this.initial;
+
+  constructor() {
+    this.getUser();
+  }
+
+  public getUser() {
+    const user = localStorage.getItem('user');
+    if (user) {
+      this.data = JSON.parse(user);
+      return this.data;
+    }
+    return null;
+  }
+
+  public setUser(data: ISingInData) {
+    this.data = data;
+    localStorage.setItem('user', JSON.stringify(data));
+  }
+
+  public removeUser() {
+    this.data = this.initial;
+    localStorage.removeItem('user');
+  }
+
+  public isLogin() {
+    if (this.data.accessToken === '') return false;
+    return true;
   }
 
 }

3080aa8f64ea28255bbde080ce46731dd5fa119a
abi-hong <flffjakfwm20@gmail.com>
2024-04-20 15:29:05 +0900

    fix(review): 리뷰 상세보기 데이터 연결

--- pages/file/components/file-detail/file-detail.component.ts
+++ pages/file/components/file-detail/file-detail.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GlobalService } from '../../../../shared/services/global.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-file-detail',
   templateUrl: './file-detail.component.html',
   styleUrl: './file-detail.component.scss'
 })
-export class FileDetailComponent {
+export class FileDetailComponent implements OnInit {
   public currentDate!: string;
   public name: string = 'filename.pdf';
 
-  constructor(public globalService: GlobalService) {
+  constructor(
+    private router: Router,
+    public globalService: GlobalService) {
     this.currentDate = this.globalService.dateService.getCurrentDateStr();
+  }
+
+  ngOnInit() {
+    // if (this.router.getCurrentNavigation() && this.router.getCurrentNavigation().extras.state) {
+    //   this.data = this.router.getCurrentNavigation().extras.state['data'];
+    // }
   }
 }
